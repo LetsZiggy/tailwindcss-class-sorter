10f656d222611c43a80a8326775c0f4992faddde
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2023-09-13 15:48:47 +0800

    chore: project settings

---
+++ generator-order-list/prettier.config.js
@@ -0,0 +1,62 @@
+/**
+ * @type {import("prettier").Config}
+ * @see https://prettier.io/docs/configuration
+ */
+export default {
+	// ---Plugins--- //
+
+	plugins: [
+		"prettier-plugin-organize-attributes",
+		"prettier-plugin-pkg",
+		"prettier-plugin-tsconfig",
+		// "prettier-plugin-tailwindcss",
+	],
+
+	// ---Options--- //
+
+	// printWidth: 120,
+	// tabWidth: 2,
+	// useTabs: true,
+	semi: false,
+	singleQuote: false,
+	quoteProps: "consistent",
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	objectWrap: "preserve",
+	bracketSameLine: true,
+	arrowParens: "always",
+	rangeStart: 0,
+	rangeEnd: Number.POSITIVE_INFINITY,
+	// parser: "",
+	// filepath: "",
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: "preserve",
+	htmlWhitespaceSensitivity: "css",
+	vueIndentScriptAndStyle: false,
+	// endOfLine: "lf",
+	embeddedLanguageFormatting: "auto",
+	singleAttributePerLine: true,
+
+	// ---prettier-plugin-organize-attributes--- //
+
+	attributeGroups: ["$DEFAULT"],
+	attributeSort: "ASC",
+	attributeIgnoreCase: true,
+
+	// ---prettier-plugin-tailwindcss--- //
+
+	// tailwindConfig: "./tailwind.config.js",
+
+	// ---Overrides--- //
+
+	/*
+	overrides: [
+		{
+			files: ["*.html"],
+			options: {},
+		},
+	],
+	*/
+}
